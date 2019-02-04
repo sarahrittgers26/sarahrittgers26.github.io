c23f4b4fb9c968c6e2ec4932c96c27d11ddc1735
sarahrittgers26 <s.r.rittgers@gmail.com>
2019-02-04 00:04:25 -0500

    smooth scroll to top

---
+++ script/modals.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#pennbookImg").on('click',function () {
+    $('#pennBookModal').modal('show'); 
+  });
+});
+$(document).ready(function () {
+  $("#studyConnectImg").on('click',function () {
+    $('#studyConnectModal').modal('show'); 
+  });
+});
+$(document).ready(function () {
+  $("#floodImg").on('click',function () {
+    $('#floodModal').modal('show'); 
+  });
+});
+$(document).ready(function () {
+  $("#lostAndFoundImg").on('click',function () {
+    $('#lostAndFoundModal').modal('show'); 
+  });
+});
